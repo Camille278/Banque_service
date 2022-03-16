7d7e51a82d364b95ff1f1c3792f21473adced42c
Camille <beirao.camille@gmail.com>
2022-03-16 15:58:42 +0100

    Tests

--- src/main/java/org/miage/banqueservice/boundary/ClientRepresentation.java
+++ src/main/java/org/miage/banqueservice/boundary/ClientRepresentation.java
@@ -4,16 +4,13 @@ import org.miage.banqueservice.Repository.ClientRessource;
 import org.miage.banqueservice.entity.Client;
 import org.miage.banqueservice.entity.Reponse;
 import org.springframework.core.env.Environment;
-import org.springframework.hateoas.server.ExposesResourceFor;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 @RestController
-//@RequestMapping(value="/clients", produces= MediaType.APPLICATION_JSON_VALUE)
-//@ExposesResourceFor(Client.class)
 public class ClientRepresentation {
 
     private Environment environment;
@@ -31,15 +28,16 @@ public class ClientRepresentation {
     }
 
     @GetMapping(value= "/clients/{idClient}/prix/{prix}/payer")
+    @Transactional
     public Reponse payerCompteClient(@PathVariable("idClient") String id, @PathVariable("prix") Float prix){
         Optional<Client> toUpdate = cr.findById(id);
         String reponseMessage;
-        Client toSave = toUpdate.get();
         Reponse toReturn = new Reponse();
 
         if(toUpdate.isEmpty()){
             toReturn.setMessage("Client inexistant");
         }else{
+            Client toSave = toUpdate.get();
             if(prix <= toUpdate.get().getMontantCompte()){
                 reponseMessage = "Financement effectué";
                 toSave.setMontantCompte(toUpdate.get().getMontantCompte()-prix);
---
+++ src/test/java/org/miage/banqueservice/ClientRepresentationTests.java
@@ -0,0 +1,93 @@
+package org.miage.banqueservice;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.miage.banqueservice.Repository.ClientRessource;
+import org.miage.banqueservice.entity.Client;
+import org.miage.banqueservice.entity.Reponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.Optional;
+
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestPropertySource(locations = "classpath:application.properties")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ClientRepresentationTests {
+
+    @LocalServerPort
+    int port;
+
+    @Autowired
+    ClientRessource cr;
+
+    @MockBean
+    Reponse reponse;
+
+    @BeforeEach
+    public void setupContext() {
+        cr.deleteAll();
+        RestAssured.port = port;
+    }
+
+    @Test
+    @DisplayName("Payer une réservation")
+    public void payerReservation() {
+        Client c1 = new Client("1", "Beirao", 140.00F);
+        cr.save(c1);
+
+        Response response = when().get("/clients/"+c1.getId()+"/prix/100.00/payer").then().statusCode(HttpStatus.SC_OK)
+                .extract().response();
+        String jsonAsString = response.asString();
+        assertThat(jsonAsString, containsString("Financement effectué"));
+    }
+
+    @Test
+    @DisplayName("Mise à jour du montant du compte")
+    public void majMontant() {
+        Client c1 = new Client("1", "Beirao", 140.00F);
+        cr.save(c1);
+
+        Response response = when().get("/clients/"+c1.getId()+"/prix/100.00/payer").then().statusCode(HttpStatus.SC_OK)
+                .extract().response();
+
+
+        Optional<Client> client = cr.findById("1");
+        assertEquals(40.00F, client.get().getMontantCompte());
+    }
+
+    @Test
+    @DisplayName("Paiement impossible")
+    public void impossibilitePaiement() {
+        Client c1 = new Client("1", "Beirao", 0.00F);
+        cr.save(c1);
+
+        Response response = when().get("/clients/"+c1.getId()+"/prix/100.00/payer").then().statusCode(HttpStatus.SC_OK)
+                .extract().response();
+        String jsonAsString = response.asString();
+        assertThat(jsonAsString, containsString("Financement impossible"));
+    }
+
+    @Test
+    @DisplayName("Client inexistant")
+    public void clientInexistant() {
+
+        Response response = when().get("/clients/1/prix/100.00/payer").then().statusCode(HttpStatus.SC_OK)
+                .extract().response();
+
+        String jsonAsString = response.asString();
+        assertThat(jsonAsString, containsString("Client inexistant"));
+    }
+}
